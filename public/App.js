e142bdb8a8aa4cabae51cf81c133b8d22b3a60f8
Mouad <mouad123b@gmail.com>
2019-10-05 18:07:13 +0100

    add predictions

--- public/App.js
+++ public/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
             const loadlModelPromise = cocoSsd.load();
 
             Promise.all([loadlModelPromise, webCamStream]).then(val => {
-                this.detectFromVideoFrame(val[0], this.videoRef.current);
+                this.detectFromVideoFrame(val[0], this.video.current);
             }).catch(error => {
                 console.error(error);
             });
@@ -38,8 +38,8 @@ class App extends React.Component {
     }
 
     detectFromVideoFrame = (model, video) => {
-        model.detect(video).then(() => {
-            
+        model.detect(video).then(predictions => {
+            this.showDetections(predictions);
 
             requestAnimationFrame(() => {
                 this.detectFromVideoFrame(model, video);
@@ -49,6 +49,41 @@ class App extends React.Component {
             console.error(error)
         });
     };
+
+    showDetections = predictions => {
+        const ctx = this.canvas.current.getContext("2d");
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        const font = "24px helvetica";
+        ctx.font = font;
+        ctx.textBaseline = "top";
+
+        predictions.forEach(prediction => {
+            console.log(prediction);
+    
+
+            const x = prediction.bbox[0];
+            const y = prediction.bbox[1];
+            const width = prediction.bbox[2];
+            const height = prediction.bbox[3];
+            // Draw the bounding box.
+            ctx.strokeStyle = "#2fff00";
+            ctx.lineWidth = 1;
+            ctx.strokeRect(x, y, width, height);
+            // Draw the label background.
+            ctx.fillStyle = "#2fff00";
+            const textWidth = ctx.measureText(prediction.class).width;
+            const textHeight = parseInt(font, 10);
+            // draw top left rectangle
+            ctx.fillRect(x, y, textWidth + 10, textHeight + 10);
+            // draw bottom left rectangle
+            ctx.fillRect(x, y + height - textHeight, textWidth + 15, textHeight + 10);
+
+            // Draw the text last to ensure it's on top.
+            ctx.fillStyle = "#000000";
+            ctx.fillText(prediction.class, x, y);
+            ctx.fillText(prediction.score.toFixed(2), x, y + height - textHeight);
+        });
+    }
 
     render() {
         return (
